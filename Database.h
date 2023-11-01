4293598d70ff620741cfe9eeb84ce4e6e7a30325
jxxyx <jxxyx21@gmail.com>
2023-11-01 14:03:38 +0800

    put new folders and createdDB.h

---
+++ Database.h
@@ -0,0 +1,14 @@
+// This are the headers that are required for the database.h
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+#include <ctype.h>
+
+// Defining structure to store key and value(s)
+typedef struct key_value {
+    char *key;
+    char *value;
+};
+
+
+
