1c574ec76c37e61ac53cb5b80aa6cdef630ea580
jxxyx <jxxyx21@gmail.com>
2023-11-03 00:45:18 +0800

    edits

--- Database.h
+++ Database.h
@@ -1,7 +1,6 @@
 // This are the headers that are required for the database.h
 #include <stdio.h>
 #include <string.h>
-#include <conio.h>
 #include <ctype.h>
 
 // This is a guard to prevent header file form being use too many times in the same compilation unit
