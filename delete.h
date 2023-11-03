2b245c323e5bc38026a8fa4c388de407dad4624b
shepherd333 <worldwithoutaking@gmail.com>
2023-11-04 01:24:49 +0800

    created update function c and header file

---
+++ delete.h
@@ -0,0 +1,9 @@
+#ifndef DELETE_H
+#define DELETE_H
+
+#include "Database.h" // Include the appropriate header for your HashMap data structure
+
+// Function prototype to delete a row from the hash map-based database
+void deleteRow(struct HashMap* hashMap, const char* idToDelete);
+
+#endif // DELETE_H
