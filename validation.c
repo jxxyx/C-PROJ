56f843322428d843d80d06d69613221d1259e83e
shepherd333 <julianteobiz@gmail.com>
2023-11-17 00:37:38 +0800

    Error handling code (untested)

--- validation.c
+++ validation.c
@@ -4,97 +4,96 @@
 #include <string.h>
 #include <ctype.h>
 
-// Placeholder for a function to check if an RFID is already in the database
-int isRFIDAlreadyInDatabase(const char* rfid) {
-    // Simulated check for duplicate RFID (you should implement this according to your database)
-    if (strcmp(rfid, "AB1234CD") == 0) {
-        return 1; // RFID exists in the database
-    }
-    return 0; // RFID is not in the database
-}
-
-// Placeholder for a function to check if an airport name is already in the database
-int isAirportNameAlreadyInDatabase(const char* airportName) {
-    // Simulated check for duplicate airport name (you should implement this according to your database)
-    if (strcmp(airportName, "JFK") == 0) {
-        return 1; // Airport name exists in the database
-    }
-    return 0; // Airport name is not in the database
-}
-
 int containsSpecialCharacter(const char* str) {
-    int length = strlen(str);
-    for (int i = 0; i < length; i++) {
+    for (int i = 0; str[i] != '\0'; i++) {
         if (!(isalnum(str[i]) || str[i] == ' ')) {
-            return 1; // Special character found
+            return 4; // Special character found
         }
     }
     return 0; // No special characters found
 }
 
-int containsDigits(const char* str) {
-    int length = strlen(str);
-    for (int i = 0; i < length; i++) {
-        if (isdigit(str[i])) {
-            return 1; // Digit found
+int containsLetter(const char* str) {
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (isalpha(str[i])) {
+            return 5; // Character found
         }
     }
-    return 0; // No digits found
+    return 0; // No Character found
 }
 
-int containslowercase(const char* str) {
-    int length = strlen(str);
-    for (int i = 0; i < length; i++) {
+void convertToUppercase(char* str) {
+    for (int i = 0; str[i] != '\0'; i++) {
         if (islower(str[i])) {
-            return 1; // lowercase found
+            str[i] = toupper(str[i]); // Convert to uppercase
         }
     }
-    return 0; // No lowercase found
 }
 
-int validateRFID(const char* rfid) {
-    // Check RFID length and format
-    if (strlen(rfid) != 8) {
-        return INVALID_INPUT;
+void removeWhitespace(char* str) {
+    int i = 0, j = 0;
+    while (str[i]) {
+        if (!isspace(str[i])) {
+            str[j++] = str[i];
+        }
+        i++;
     }
+    str[j] = '\0'; // Null-terminate the new string
+}
+
+int validateRFID(char* RFIDValue) {
+    // Null check
+    if (RFIDValue == NULL) {
+        return 3;
+    }
+
+    // Remove possible whitespace
+    removeWhitespace(RFIDValue);
 
     // Check for special characters
-    if (containsSpecialCharacter(rfid)) {
-        return INVALID_INPUT;
+    if (containsSpecialCharacter(RFIDValue)) {
+        return 4;
+    }
+    // Check for letters
+    if (containsLetter(RFIDValue)) {
+        return 5;
     }
 
-    // Check if RFID is unique (simulated)
-    if (isRFIDAlreadyInDatabase(rfid)) {
-        return DUPLICATE_KEY;
+    // Check if string is empty
+    if (RFIDValue[0] == '\0') {
+        return 6;
     }
 
     return VALID_INPUT;
 }
 
-int validateAirportName(const char* airportName) {
+int validateAirportName(char* Location) {
+    // Null check
+    if (Location == NULL) {
+        return 3;
+    }
+
+    // Remove possible whitespace
+    removeWhitespace(Location);
+
+    // Convert to uppercase
+    convertToUppercase(Location);
+
     // Check airport name length
-    if (strlen(airportName) < 3 || strlen(airportName) > 3) {
-        return INVALID_INPUT;
+    if (strlen(Location) != 3) {
+        return 8;
     }
 
-    // Check for special characters
-    if (containsSpecialCharacter(airportName)) {
-        return INVALID_INPUT;
+    // Check for non-alphabetic characters
+    for (int i = 0; i < 3; i++) {
+        if (!isalpha(Location[i])) {
+            return 9;
+        }
     }
 
-    // Check for digits
-    if (containsDigits(airportName)) {
-        return INVALID_INPUT;
-    }
-
-    // Check for lowercase
-    if (containslowercase(airportName)) {
-        return INVALID_INPUT;
-    }
-
-    // Check if airport name is unique (simulated)
-    if (isAirportNameAlreadyInDatabase(airportName)) {
-        return DUPLICATE_KEY;
+    // Check if string is empty
+    if (Location[0] == '\0') {
+        return 6;
     }
 
     return VALID_INPUT;
@@ -102,14 +101,35 @@ int validateAirportName(const char* airportName) {
 
 void handleError(int errorCode) {
     switch (errorCode) {
-        case INVALID_INPUT:
-            printf("Invalid input.\n");
+        case VALID_INPUT:
+            printf("Input is valid.\n");
+            break;
+        case NULL_INPUT:
+            printf("Input is null.\n");
+            break;
+        case CONTAINS_SPECIAL_CHAR:
+            printf("Input contains special characters.\n");
+            break;
+        case CONTAINS_LETTER:
+            printf("Input contains letters.\n");
+            break;
+        case EMPTY_STRING:
+            printf("Input is an empty string.\n");
+            break;
+        case WHITESPACE_ONLY:
+            printf("Input contains only whitespace.\n");
+            break;
+        case INVALID_AIRPORT_NAME_LENGTH:
+            printf("Airport name is of invalid length.\n");
+            break;
+        case NON_ALPHABETIC_AIRPORT_NAME:
+            printf("Airport name contains non-alphabetic characters.\n");
             break;
         case DUPLICATE_KEY:
             printf("Duplicate key found.\n");
             break;
         default:
-            printf("An error occurred.\n");
+            printf("An unknown error occurred.\n");
             break;
     }
 }
--- validation.h
+++ validation.h
@@ -1,8 +1,8 @@
 #ifndef VALIDATION_H
 #define VALIDATION_H
 
-int validateRFID(const char* rfid);
-int validateAirportName(const char* airportName);
+int validateRFID(char* rfid);
+int validateAirportName(char* airportName);
 void handleError(int errorCode);
 
 #endif
