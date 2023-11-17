e8468f74f3ba3d0821a9e3fc576752eb8a437b56
jxxyx <jxxyx21@gmail.com>
2023-11-17 21:52:03 +0800

    main

---
+++ Main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "Database.h"
+#include "delete.h"
+#include "update.h"
+#include "query.h"
+#include "fileio.h"
+#include "showall.h"
+#include "insert.h"
+#include "validation.h"
+
+int main(){
+    
+}
