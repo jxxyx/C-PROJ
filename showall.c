b088f0c1896c29c389b9488034c658f157b99a1b
Yusri <144083812+flamingoMoose@users.noreply.github.com>
2023-11-18 18:51:30 +0800

    Revert "menu2 and menu1"
    This reverts commit aaf7a444615fe66ae87d9670f016bd2aafab3eb3.

--- showall.c
+++ showall.c
@@ -3,20 +3,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Create a function to read DB txt file
+// Create a function to read DB txt file 
 
-void showAll(BaggageTable myDatabase) {
-    int count = 0;
-    printf("\nRFIDValue : Location\n");
-    for (int i = 0; i < myDatabase.size; i++) {
-        Baggage *baggage = myDatabase.table[i];
-        while (baggage != NULL) {
-            printf("RFID %4s : %s\n", baggage->RFIDValue, baggage->Location);
-            baggage = baggage->next;
-            count++;
-            }
-        }
-    printf("\n-----------------------------\n");
-    printf("Total Record(s) Found: %d\n", count);
-    printf("-----------------------------\n");
+void readBaggageTable() {
+    FILE *file = fopen("BaggageInfoEzDB.txt", "r");
+    if (file == NULL) {
+        printf("Could not open file to read\n");
+        return;
+    }
+
+    char ch;
+    while((ch = fgetc(file)) != EOF) {
+        putchar(ch);
+    }
+
+    fclose(file);
 }
+
